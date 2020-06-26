01e6364d9f65fa5599660e2d88fb6f70f86f2d74
Maeva Wolff <maeva.wolff@hetic.net>
2020-06-26 05:33:45 +0200

    import json file in app.vue for test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import test from './data/test.json';
 
 function App() {
-  return <div className='App'></div>;
+  console.log(test);
+  return (
+    <div className='App'>
+      {test.map(el => {
+        return <h1>{el.title}</h1>;
+      })}
+    </div>
+  );
 }
 
 export default App;
