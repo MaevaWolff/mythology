d82d08f2ce2f8acada03730977a1f60950cd0254
Maeva Wolff <maeva.wolff@hetic.net>
2020-07-04 23:00:41 +0200

    add function to hide loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import './Loader.scss';
 
 export default class Loader extends Component {
+  constructor() {
+    super();
+    this.state = {
+      visible: true
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        visible: false
+      });
+      console.log(this.state.visible);
+    }, 3000);
+  }
+
   render() {
     return (
-      <div className='loader'>
+      <div className={this.state.visible ? 'loader' : 'loader none'}>
         <div className='loader__content'>
           <h1 className='loader__title'>ZEUS.</h1>
           <svg
